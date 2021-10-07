84e4d03117710aa02f7cf879803d0eb62db9c19f
Marcus <mlim0032@student.monash.edu>
2021-10-08 00:36:40 +0800

    update partial complete code

--- node.c
+++ node.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <memory.h>
 #include <time.h>
+#include <unistd.h>
 
 #define DISP 1
 #define SHIFT_ROW 0
@@ -13,13 +15,15 @@
 
 
 /* This is the slave; each slave/process simulates one tsunameter sensor node */
-int node_io(MPI_Comm world_comm, MPI_Comm comm, int dims[]){
-
+int node_io(MPI_Comm world_comm, MPI_Comm comm, int dims[], int threshold, int inputIterBaseStation){
+    printf("in node\n");
     int ndims=2, size, my_rank, reorder, my_cart_rank, ierr, masterSize;
+    float randNum, mAvg;
 	MPI_Comm comm2D;
 	int coord[ndims];
 	int wrap_around[ndims];
     int nbr_i_lo, nbr_i_hi, nbr_j_lo, nbr_j_hi; // stores rank of top, bottom, left, right neighbour respectively
+    float* ma_arr=NULL;
 
     MPI_Comm_size(world_comm, &masterSize); // size of the master communicator
   	MPI_Comm_size(comm, &size); // size of the slave communicator
@@ -39,16 +43,67 @@ int node_io(MPI_Comm world_comm, MPI_Comm comm, int dims[]){
         printf("ERROR[%d] creating CART\n",ierr);
 
     // find my coordinates in the cartesian communicator group 
-	MPI_Cart_coords(comm2D, my_rank, ndims, coord); // coordinates are returned into the 'coord' array
+	MPI_Cart_coords(comm2D, my_rank, ndims, coord); 
 	// use my cartesian coordinates to find my rank in cartesian group
 	MPI_Cart_rank(comm2D, coord, &my_cart_rank);
 
-    // Returns the shifted source and destination ranks, given a shift direction and amount
-	// Here, DISP=1 --> look at the immediate neighbours only (your direct top/down/left/right neighbours only)
+    // Returns the shifted source and destination ranks, given a shift direction and displacement of 1
 	MPI_Cart_shift( comm2D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi );
 	MPI_Cart_shift( comm2D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi );
 
+    printf("before do and my rank is: %d\n", my_rank);
+    printf("before do and my cart rank is: %d\n", my_cart_rank);
+
+    // array to store the values generated to calculate moving average (MA)
+    ma_arr = (float*)malloc(inputIterBaseStation * sizeof(float));
+    memset(ma_arr, 0, inputIterBaseStation * sizeof(float));
+
+    // double *pX4Buff = NULL;
+    // pX4Buff = (double*)malloc(fileElementCount * sizeof(double));
+	// memset(pX4Buff, 0, fileElementCount * sizeof(double)); // Optional
+
+
     
+
+    int counter = 0, i =0;
+    double startTime, endTime;
+    
+    do{
+        
+        startTime = MPI_Wtime();
+
+        // reference to generate a random float between two floats: https://stackoverflow.com/a/13409005/16454185
+        // Here, we generate a value within [5000, threshold+500]
+        randNum = ((threshold+500 - 5000) * ((float)rand() / RAND_MAX)) + 5000;
+        ma_arr[counter] = randNum;
+
+        // calculate MA
+        float sum = 0;
+        for (i=0; i < (counter+1); i++)
+            sum += ma_arr[i];
+        mAvg = (float) sum / (counter+1);
+        // printf("%f\n", mAvg);
+
+        // wait for all nodes to complete computing their MA, then only we check if MA > threshold
+        MPI_Barrier(comm); //or 'comm' not sure
+
+        if (mAvg > threshold){
+            printf("%f, hi\n", mAvg);
+        }
+
+
+        endTime = MPI_Wtime();
+        // if whole operation in that iteration is less than 10 seconds, delay it to 10 seconds before next iteration
+        if((endTime - startTime) <=10){
+            sleep(10 - (endTime-startTime));
+        }
+
+        counter++;
+    }
+    while (counter<inputIterBaseStation);
+
+
+
     // receive termination signal
     char buf[256]; // temporary
     MPI_Status status;
@@ -61,7 +116,7 @@ int node_io(MPI_Comm world_comm, MPI_Comm comm, int dims[]){
 
 
 
-
+    free(ma_arr);
     MPI_Comm_free( &comm2D );
 	return 0;
 
