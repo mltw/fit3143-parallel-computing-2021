25197c26f57ce5585a45fea8ff268950f1b082c4
kexin22 <tankexin31@gmail.com>
2021-10-18 00:20:23 +0800

    tidied up code

--- base_station.c
+++ base_station.c
@@ -1,3 +1,10 @@
+/*
+FIT3143 S2 2021 Assignment 2 
+Topic: Tsunami Detection in a Distributed Wireless Sensor Network (WSN)
+Group: MA_LAB-04-Team-05
+Authors: Tan Ke Xin, Marcus Lim
+*/
+
 #include "header.h" 
 #include <stdio.h>
 #include <math.h>
@@ -14,12 +21,14 @@
 #define MSG_SHUTODWN_BASE_STATION_THREAD 2
 #define MSG_ALERT_BASE_STATION 3
 
+// struct to store no of rows & no of cols received from main function
 struct sizeGrid
 {
     int recvRows;
     int recvCols;
 };
 
+// struct to store arguments for reporting alerts 
 struct arg_struct_base_station {
     int iteration;
     char timestamp[256];
@@ -32,23 +41,25 @@ struct arg_struct_base_station {
     float recv_node_ma_arr[4];
     int recv_node_coord[4][2];
 
-    // char reporting_node_ip_add[256];
-    // char recv_node_ip_add[4][256];
 };
 
+// struct to store randomly generated coordinates 
 struct coordinates {
     int x;
     int y;
 };
 
-// reference for getting current time: https://www.cplusplus.com/reference/ctime/localtime/
+// struct to store neccessary data for altimeter 
 struct globalData {
     char timestamp[256];
     struct coordinates randCoord;
     float randFloat;
 };
+
+// global shared array 
 struct globalData globalArr[5]; 
 
+// initialize global variable 
 int sharedGlobalSignal = -1;
 int thresholdGlobal;
 int nrowsGlobal, ncolsGlobal;
@@ -57,13 +68,7 @@ pthread_mutex_t mutex2;
 
 /* This is the base station, which is also the root rank; it acts as the master */
 int base_station_io(MPI_Comm world_comm, MPI_Comm comm, int inputIterBaseStation, int threshold, int nrows, int ncols){
-    /* TODO:      
-      - before program terminates, generate summary
-         - no of alerts (true & false)
-         - avg comm time
-         - time taken for base station 
-      
-    */
+    // initialize neccessary variables 
     nrowsGlobal = nrows;
     ncolsGlobal = ncols;
     thresholdGlobal = threshold;
@@ -81,12 +86,10 @@ int base_station_io(MPI_Comm world_comm, MPI_Comm comm, int inputIterBaseStation
     void *resSignal;
     
     struct sizeGrid val = { nrows, ncols};
-    // printf("received no of rows is %d\n", val.recvRows);
-    // printf("received no of col is %d\n", val.recvCols);
     
-    // thread for altimeter
+    // Thread for altimeter
     pthread_t tid;
-    thread_init = pthread_create(&tid, 0, altimeter, &val); // Create the thread
+    thread_init = pthread_create(&tid, 0, altimeter, &val);                     // Create the thread
     if (thread_init != 0)
         printf("Error creating altimeter thread in base station\n");
     
@@ -96,32 +99,26 @@ int base_station_io(MPI_Comm world_comm, MPI_Comm comm, int inputIterBaseStation
     //if (thread_init != 0)
         //printf("Error creating awaiting user input thread in base station\n");
 
-    // thread to send/receive msg to/from sensor nodes
+    // Thread to receive msg to/from sensor nodes
     pthread_t tid3;
     thread_init = pthread_create(&tid3, NULL, base_station_recv, NULL);
     if (thread_init != 0)
         printf("Error creating base station recv thread in base station\n");
 
-    // run for a fixed number of iterations which is set during compiled time
+    // Run for a fixed number of iterations which is set during compiled time
     for (int i =0; i <= inputIterBaseStation;i++){
          
          if (i == inputIterBaseStation){
-             // send termination signal to nodes & altimeter to shutdown gracefully
+             // send termination signal to nodes & altimeter to shutdown 
              for (int j = 1; j <= nNodes; j++){
                 MPI_Send(buf, 0, MPI_CHAR, j, MSG_SHUTDOWN, world_comm);
-                
              }
              
-             
+             // update global shared var, so in altimeter will know value being updated to 1, time to terminate
+             // pthread_mutex_lock : prevent race condition  
              pthread_mutex_lock(&mutex);
-             printf("Changing the shared resource now.\n");
-             printf("GLOBAL SIGNAL PREVIOUSLY IS %d\n", sharedGlobalSignal);
              sharedGlobalSignal = 1;
-             printf("GLOBAL SIGNAL NOW IS %d\n", sharedGlobalSignal);
-             pthread_mutex_unlock(&mutex);
-             printf("MUTEX UNLOCKED\n");
-             
-                 
+             pthread_mutex_unlock(&mutex);  
          }
          else{
             sleep(10);
@@ -129,7 +126,8 @@ int base_station_io(MPI_Comm world_comm, MPI_Comm comm, int inputIterBaseStation
       
     }
 
-    pthread_join(tid, NULL);                                    // Wait for the thread to complete
+    // Wait for the thread to complete
+    pthread_join(tid, NULL);                                    
     //pthread_join(tid2, &resSignal);
     pthread_join(tid3, NULL); 
 
@@ -139,59 +137,37 @@ int base_station_io(MPI_Comm world_comm, MPI_Comm comm, int inputIterBaseStation
         //}
     //else 
         //printf("I DID NOT RECV SIGNAL YET\n");
-
-     
-     // last pthread should return received node info
-     int length = sizeof(globalArr)/sizeof(globalArr[0]);    
-     //printf("length of global Arr is %d\n", length);
-     for (int k= length-1;k>=0;k--){
-        //printf("arr at index %d\n", k);
-        printf("time at index %d is %s\n",k,globalArr[k].timestamp);
-        printf("random coordinates at index %d is (%d,%d)\n",k ,globalArr[k].randCoord.x,globalArr[k].randCoord.y);
-        printf("random float at index  %d is %.3f\n",k, globalArr[k].randFloat );
- 
-     }
-    
     return 0;
 }
 
 
+/*This is a thread spawned by the base station. */
 void* altimeter(void *pArg) 
 {   
+    // initialize neccessary variables 
     struct sizeGrid *arg = (struct sizeGrid *) pArg;
-    // printf("ALTIMETER recv row is %d, recv col is %d\n", arg->recvRows, arg->recvCols);
-
     char buf[256]; 
     MPI_Status status;
     MPI_Request receive_request;
     double startTime, endTime,elapsed;
     int maxSize = 5, current=0,i=0;
     
-
-
-    // continue update the global array until we receive termination signal
+    // Continue update the global array until we receive termination signal
     while (1){
         startTime = MPI_Wtime();
         
         pthread_mutex_lock(&mutex);
         if (sharedGlobalSignal == 1){
-            printf("NOW I WILL STOP ALTIMETER\n");
+            // signal to stop altimeter received, break for loop & exit 
             pthread_mutex_unlock(&mutex);
-            printf("MUTEX UNLOCKED IN ALTIMETER\n");
             break;
         }
-        else{
-            printf("GLOBAL SIGNAL RECEIVED SIGNAL MUTEX IN ALTIMETER IS STILL%d\n", sharedGlobalSignal);
-            }
-            pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);
             
         
-  
-       
         if (current <= maxSize-1){
-            // still can insert
-            printf("in if and i is: %d\n",i);
-            processFunc(current, (arg->recvRows), (arg->recvCols));               // call helper func to fill the array
+            // Still have slots to insert
+            processFunc(current, (arg->recvRows), (arg->recvCols));               // Call helper func to fill the array
             current++;    
             i++;                                           
         }
@@ -199,7 +175,6 @@ void* altimeter(void *pArg)
             // once we reset, then even the next iter will update in the correct place
             // e.g.: first time array full, then reset current to 0, now current will be 1 (but array is still full)
             // so we next will remove the 2nd earliest data (which is at index 1)
-            printf("ARRAY FULL\n");
             current = 0;                                                         // reset, start from index 0 again 
             processFunc(current, (arg->recvRows), (arg->recvCols));
             current++;
@@ -210,33 +185,30 @@ void* altimeter(void *pArg)
        endTime = MPI_Wtime();
        elapsed = endTime - startTime;
        
-        // printf("start time is %.2f\n", startTime);
-        // printf("end time is %.2f\n", endTime);
-        printf("Time elapsed for iteration %d is %.5f\n", i-1, endTime - startTime);
          // if whole operation in that iteration is less than 5 seconds, delay it to 5 second before next iteration
        if( elapsed <= 5){
-            // printf("delayedddddd at iteration %d for %.5f seconds\n",i,5-elapsed);
             sleep(5 - elapsed);
-            //printf("SLEPT FOR iteration %d\n", i);
        }
         printf("--------------------------------------\n");
         
     }
 
-    printf("bye from altimeter thread\n");
     pthread_exit (NULL);
     return 0;
 }
 
+/* This is a helper function to aid in processing neccessary values for altimeter, will be called each iteration*/
 void processFunc(int counter, int recvRows, int recvCols){
 
+    // ensure only 1 process can access & modify shared global resource at a time 
     pthread_mutex_lock(&mutex2);
-    printf("INSERTING INTO ARRAY, UPDATING\n");
     int maxLimit = 9000;
 
     srand(time(NULL));
     
     printf("------ Altimeter Iteration %d ---------\n", counter);
+
+    // reference for getting current time: https://www.cplusplus.com/reference/ctime/localtime/
     // Get time generated
     time_t rawtime;
     struct tm * timeinfo;
@@ -245,9 +217,6 @@ void processFunc(int counter, int recvRows, int recvCols){
     sprintf(globalArr[counter].timestamp, "%s", (asctime(timeinfo)));
     printf("Current date and time is %s\n",globalArr[counter].timestamp);
    
- 
-    
-    
     // Generate random coordinates 
     globalArr[counter].randCoord.x = rand() % recvRows;
     globalArr[counter].randCoord.y = rand() % recvCols;
@@ -257,7 +226,6 @@ void processFunc(int counter, int recvRows, int recvCols){
      globalArr[counter].randFloat = ((maxLimit -thresholdGlobal)*  ((float)rand() / RAND_MAX)) + thresholdGlobal;
      printf("Random float of iteration %d is %.3f\n",counter, globalArr[counter].randFloat );
      pthread_mutex_unlock(&mutex2);
-     printf("DONE INSERTION, NOW MUTEX UNLOCK FOR SHARED GLOBAL ARR\n");
         
 }
 
@@ -270,9 +238,6 @@ void* userInput(void *pArg){
 	char buffer[MAX_LENGTH + 1];
     memset (buffer, 0, MAX_LENGTH + 1);
     
-    
-      
-	
 	/* Read a line of input from STDIN */
 	//while (1)
     while (fgets (buffer, MAX_LENGTH, stdin) != NULL)
@@ -296,18 +261,16 @@ void* userInput(void *pArg){
     return 0;
 }
 
-// base station POSIX thread to send/receive msg to/from sensor nodes
+/* A base station POSIX thread to receive msg from sensor nodes */
 void* base_station_recv(void *arguments){
+    // initialize neccessary variables 
     MPI_Status status;
     int recv, i;
-
     FILE *pFile;
 
     // create a custom MPI Datatype for the struct that will be sent from nodes, and received here in base station 
     struct arg_struct_base_station base_station_args;
     MPI_Datatype Valuetype;
-    //  char reporting_node_ip_add[256];
-    // char recv_node_ip_add[4][256];
     MPI_Datatype datatype[8] = { MPI_INT, MPI_CHAR, MPI_INT, MPI_FLOAT, MPI_INT, MPI_INT, MPI_FLOAT, MPI_INT};
     int blocklen[8] = {1, 256, 1, 1, 2, 4, 4, 8};
     MPI_Aint disp[8];
@@ -319,8 +282,6 @@ void* base_station_recv(void *arguments){
     MPI_Get_address(&base_station_args.recv_node_rank_arr, &disp[5]);
     MPI_Get_address(&base_station_args.recv_node_ma_arr, &disp[6]);
     MPI_Get_address(&base_station_args.recv_node_coord, &disp[7]);
-    // MPI_Get_address(&base_station_args.reporting_node_ip_add, &disp[8]);
-    // MPI_Get_address(&base_station_args.recv_node_ip_add, &disp[9]);
 
     for (i=7; i>=1; i--){
         disp[i] = disp[i] - disp[i-1];
@@ -333,7 +294,10 @@ void* base_station_recv(void *arguments){
 
     while (1){
         startTime = MPI_Wtime();
+
+        // receive message from ANY_SOURCE & ANY_TAG, perform appropriate actions based on tag received 
         MPI_Recv(&base_station_args, 8, Valuetype, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
         if (status.MPI_TAG == MSG_SHUTODWN_BASE_STATION_THREAD){
             printf(" base station recvied alert from node %d\n", status.MPI_SOURCE-1);
             break;
@@ -351,9 +315,8 @@ void* base_station_recv(void *arguments){
             int length = sizeof(globalArr)/sizeof(globalArr[0]);  
             pFile = fopen("base_station_log.txt", "a");
             
-            // loop through the global array to see if there are matching coordinates
+            // loop through the global array(in reverse manner) to see if there are matching coordinates
             pthread_mutex_lock(&mutex2);
-            printf("NOW COMPARING IN FOR LOOP SECOND MUTEX\n");
             for (int k= length-1; k>=0; k--){
                 if ((globalArr[k].randCoord.x == base_station_args.reporting_node_coord[0]) 
                     && (globalArr[k].randCoord.y == base_station_args.reporting_node_coord[1])){
@@ -361,22 +324,17 @@ void* base_station_recv(void *arguments){
                         if (base_station_args.reporting_node_ma >= globalArr[k].randFloat-300 
                             && base_station_args.reporting_node_ma <= globalArr[k].randFloat+300){
                                 true_alert = 1;
-                                // fprintf(pFile, "haha reporting ma: %f\n", base_station_args.reporting_node_ma);
-                                // fprintf(pFile, "haha globalArr de ma: %f\n", globalArr[k].randFloat);
-                                // fprintf(pFile, "haha globalArr de coord: (%d,%d)\n",globalArr[k].randCoord.x, globalArr[k].randCoord.y);
                                 break;
                             }
                     }
             }
             pthread_mutex_unlock(&mutex2);
-            printf("I FINISH COMPARISION FOR MUTEX 2 NOW UNLOCK IT\n");
         
+        // not sure if we should remove this 
         endTime = MPI_Wtime();
         elapsed = endTime - startTime;
         if( elapsed <= 5){
-            // printf("delayedddddd at iteration %d for %.5f seconds\n",i,5-elapsed);
             sleep(5 - elapsed);
-            //printf("SLEPT FOR iteration %d\n", i);
         }
 
 
@@ -388,19 +346,6 @@ void* base_station_recv(void *arguments){
             time_t t;
             t = time(NULL);
             char *logged_timestamp = ctime(&t);
-            // printf("%s -\n", logged_timestamp);
-            // time_t t1;
-            // t1 = time(NULL);
-            
-
-            // char C[27];
-            // ctime_r(&t1, &C);
-            
-            // time_t rawtime_log;
-            // struct tm * timeinfo_log;
-            // time(&rawtime_log);
-            // timeinfo_log = localtime (&rawtime_log);
-            // time still got bug 
 
             // time and alert type
             fprintf(pFile, "Logged Time: \t\t\t%s", logged_timestamp);
@@ -456,8 +401,6 @@ void* base_station_recv(void *arguments){
             fclose(pFile);
         }
     }
-
-    printf("bye from base station thread\n");
     MPI_Type_free(&Valuetype);
     pthread_exit(NULL);
     
